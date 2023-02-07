00d410fcfd756a2eba4f96b213bc83a6ca30734f
ayoub <elattarayoub26@domain.com>
2023-02-07 13:40:06 +0100

    add app homepage files

---
+++ src/homepage.component.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+
+const Homepage= ()=>
+<div className="homepage">
+    <div className="directory-menu">
+        <div className="menu-item">
+            <div className="content">
+<h1 className="title">HAT</h1>
+                <span>SHOP NOW</span>
+            </div>
+        </div>
+        
+
+<div className="menu-item">
+            <div className="content">
+<h1 className="title">JACKET</h1>
+                <span>SHOP NOW</span>
+            </div>
+            
+        </div>
+
+        <div className="menu-item">
+            <div className="content">
+<h1 className="title">SNEAKERS</h1>
+                <span>SHOP NOW</span>
+            </div>
+            
+        </div>
+
+        <div className="menu-item">
+            <div className="content">
+<h1 className="title">WOMEN</h1>
+                <span>SHOP NOW</span>
+            </div>
+            
+        </div>
+
+        <div className="menu-item">
+            <div className="content">
+<h1 className="title">MEN</h1>
+                <span>SHOP NOW</span>
+            </div>
+            
+        </div>
+
+    </div>
+</div>
+
+
+
+export default Homepage
